226907c72893014469a440a3146d9418d70c50de
Jon Graf <jongraf1@gmail.com>
2016-07-24 00:51:03 -0500

    Javascript will now toggle colors, status, and image

--- toggleButton.js
+++ toggleButton.js
@@ -1,9 +1,17 @@
 function toggleButton() {
  var imageLoaded = document.getElementById("ButtonImage");
+
     
-    $('#DoorStatus').html('TEST TEXT');
-    
-    if(imageLoaded.src == 'Images/GreenButton.png') {
+    if(imageLoaded.src.indexOf('GreenButton.png') > -1) {
         $('#ButtonImage').attr('src', 'Images/RedButton.png');
+        $('#header').css('background','#ff3333');
+        $('#bumper').css('background','#cc0000');
+        $('#DoorStatus').html('Status: Open');
+    }
+    else {
+        $('#ButtonImage').attr('src', 'Images/GreenButton.png');
+        $('#header').css('background','#009933');
+        $('#bumper').css('background','#006622');
+        $('#DoorStatus').html('Status: Closed');
     }
 }
