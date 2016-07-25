647c0674b51e246e2fb5252f2c4621e4714eabe6
Jon Graf <jongraf1@gmail.com>
2016-07-24 21:25:40 -0500

    Updated JS file to submit ajax request to webserver.

--- toggleButton.js
+++ toggleButton.js
@@ -1,17 +1,24 @@
 function toggleButton() {
- var imageLoaded = document.getElementById("ButtonImage");
+    	$(document).ready(function() {
+        jQuery.ajax({
+//            url: "OwnerJson.json",
+//            dataType:'json',
+            success:function(response) {
+                var imageLoaded = document.getElementById("ButtonImage");
 
-    
-    if(imageLoaded.src.indexOf('GreenButton.png') > -1) {
-        $('#ButtonImage').attr('src', 'Images/RedButton.png');
-        $('#header').css('background','#ff3333');
-        $('#bumper').css('background','#cc0000');
-        $('#DoorStatus').html('Status: Open');
-    }
-    else {
-        $('#ButtonImage').attr('src', 'Images/GreenButton.png');
-        $('#header').css('background','#009933');
-        $('#bumper').css('background','#006622');
-        $('#DoorStatus').html('Status: Closed');
-    }
+                if(imageLoaded.src.indexOf('GreenButton.png') > -1) {
+                    $('#ButtonImage').attr('src', 'Images/RedButton.png');
+                    $('#header').css('background','#ff1a1a');
+                    $('#bumper').css('background','#990000');
+                    $('#DoorStatus').html('Status: Open');
+                }
+                else {
+                    $('#ButtonImage').attr('src', 'Images/GreenButton.png');
+                    $('#header').css('background','#009933');
+                    $('#bumper').css('background','#006622');
+                    $('#DoorStatus').html('Status: Closed');
+                }
+            }
+        });
+    });
 }
