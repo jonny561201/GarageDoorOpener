cca57c1980d3b56ef1265d1de80c335df4bd32db
Jon Graf <jongraf1@gmail.com>
2016-07-30 19:21:22 -0500

    added CSS to remove image highlighting and renamed text file to javascript

--- NodeReadPin.js
+++ NodeReadPin.js
@@ -4,5 +4,7 @@ gpio.setMode(gpio.MODE_RPI);
 gpio.setup(40, gpio.DIR_IN, readInput);
 
 function readInput() {
-		gpio.read(40, function(err, value) {console.log(value)});
+		gpio.read(40, function(err, value) {
+			console.log('Pin 40 value: ' + value)
+		});
 	}
